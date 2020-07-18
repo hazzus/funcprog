af30f2aa568dc2e8bed506500f4e56fb82c25b89
Pavel Bezborodov <dfa.sro591@gmail.com>
2020-07-18 21:22:41 +0300

    hw2: adds hw2

--- hw2/app/Main.hs
+++ hw2/app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
-import Lib
+import           Control.Monad.State (execStateT)
+import           System.Directory    (makeAbsolute)
+
+import           FileSystem          (download, upload)
+import           MainArgsParser
+import           Pipeline            (interpretor)
 
 main :: IO ()
-main = someFunc
+main = do
+  dir <- execArgParserInfo >>= makeAbsolute
+  root <- upload dir
+  newRoot <- execStateT interpretor root
+  download newRoot dir
